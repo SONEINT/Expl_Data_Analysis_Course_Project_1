0db48f5562d396bc185a33382e5f007a8174739b
SONEINT <social.network.intel@me.com>
2014-06-07 12:17:32 +0200

    modify plot1.R syntax

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ source("DownloadCleanPrepare.R")
 ComputePlot1 <- function() {
     data_plot_1 <- DownloadCleanPrepareData()
     par(mfrow=c(1,1))
-    png(filename = "plot1.png", width = 480, height = 480, units = "px", bg='transparent')
+    png(filename = "plot1.png", width = 480, height = 480, units = "px", bg="transparent")
     hist(data_plot_1$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
     dev.off()
 }

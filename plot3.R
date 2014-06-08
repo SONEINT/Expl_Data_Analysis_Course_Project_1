d85c71332f360b0f6c94a82b3bf60d3498bbbbb2
SONEINT <social.network.intel@me.com>
2014-06-09 00:33:56 +0200

    Update plot3.R file
    Minor modification

--- plot3.R
+++ plot3.R
@@ -2,6 +2,7 @@ source("DownloadCleanPrepare.R")
 
 ComputePlot3 <- function() {
     data_plot3 <- DownloadCleanPrepareData()
+    par(mfrow=c(1,1))
     png(filename = "plot3.png", width = 480, height = 480, units = "px", bg="transparent")
     cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
     plot(data_plot3$DateTime, data_plot3$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
